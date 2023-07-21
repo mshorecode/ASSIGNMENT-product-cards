756f572418ae6c8356bec6a4d25d9983da5984db
Matthew Shore <mshorecode@gmail.com>
2023-07-21 12:14:57 -0500

    Makes soldOut image responsive and overlay

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const products = [
     id: 1,
     productName: "the Solid Label",
     productDescription: "Unleash the power of music with this timeless medium, perfect for audiophiles seeking warm, rich tones and an authentic listening experience. ",
-    availability: false,
+    availability: true,
     specifications: {
       size: `4" × 2½" × ½"`,
       weight: "1.43 oz",
@@ -127,10 +127,12 @@ const cardsOnDom = (array) => {
         <h2>${product.productName}</h2>
       </header>
       <div class="card-body">
-        <img src="${product.imageUrl}" class="card-img-top img" alt="${product.productName}">
-        ${!product.availability ? `<div class="container">
-          <img src="${product.soldOut}">
-        </div>` : ``}
+        <div class="product">
+          <img src="${product.imageUrl}" class="card-img-top img" alt="${product.productName}">
+          ${!product.availability ? `<div class="container container_0">
+            <img src="${product.soldOut}" class="sold-out">
+          </div>` : ``}
+        </div>
         <p class="description">${product.productDescription}</p>
         <p id="available">${product.availability ? `Available` : `Out-of-Stock`}</p>
       </div>
